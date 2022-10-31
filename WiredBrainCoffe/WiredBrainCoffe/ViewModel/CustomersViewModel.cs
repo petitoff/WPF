943f06414c8d67573607bffe765734bd5eb151b1
Błażej Domagała <petitoff@outlook.com>
2022-10-31 17:21:41 +0100

    created ViewModelBase

--- WiredBrainCoffe/WiredBrainCoffe/ViewModel/CustomersViewModel.cs
+++ WiredBrainCoffe/WiredBrainCoffe/ViewModel/CustomersViewModel.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using WiredBrainCoffe.Data;
 using WiredBrainCoffe.Model;
 
 namespace WiredBrainCoffe.ViewModel
 {
-    class CustomersViewModel : INotifyPropertyChanged
+    class CustomersViewModel : ViewModelBase
     {
         private readonly ICustomerDataProvider _customerDataProvider;
         private Customer? _selectedCustomer;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         public CustomersViewModel(ICustomerDataProvider customerDataProvider)
         {
@@ -57,11 +53,6 @@ namespace WiredBrainCoffe.ViewModel
             var customer = new Customer { FirstName = "New" };
             Customers.Add(customer);
             SelectedCustomer = customer;
-        }
-
-        private void OnPropertyChange([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
