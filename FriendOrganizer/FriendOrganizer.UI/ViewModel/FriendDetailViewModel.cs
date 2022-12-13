792274dd3868e7bfe9ebb3261c03ef0139b28f92
Błażej Domagała <petitoff@outlook.com>
2022-12-13 10:40:56 +0100

    Create the FriendDetailView

---
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
+    {
+        private Friend _friend;
+        private IFriendDataService _friendDataService;
+
+        public FriendDetailViewModel(IFriendDataService friendDataService)
+        {
+            _friendDataService = friendDataService;
+        }
+
+        public Friend Friend
+        {
+            get { return _friend; }
+            set
+            {
+                _friend = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public async Task LoadAsync(int friendId)
+        {
+            var friend = await _friendDataService.GetByIdAsync(friendId);
+        }
+    }
+}
--- FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -8,12 +8,14 @@ namespace FriendOrganizer.UI.ViewModel
     public class MainViewModel : ViewModelBase
     {
 
-        public MainViewModel(INavigationViewModel navigationViewModel)
+        public MainViewModel(INavigationViewModel navigationViewModel, IFriendDetailViewModel friendDetailViewModel)
         {
             NavigationViewModel = navigationViewModel;
+            FriendDetailViewModel = friendDetailViewModel;
         }
 
         public INavigationViewModel NavigationViewModel { get; }
+        public IFriendDetailViewModel FriendDetailViewModel { get; }
 
         public async Task LoadAsync()
         {
