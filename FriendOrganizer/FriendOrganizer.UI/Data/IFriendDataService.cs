9c285fd2317007784b0c93ba08726f9f52659ef7
Błażej Domagała <petitoff@outlook.com>
2022-12-16 01:29:43 +0100

    Save the Friend with Entity Framework

--- FriendOrganizer/FriendOrganizer.UI/Data/IFriendDataService.cs
+++ FriendOrganizer/FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -7,5 +7,7 @@ namespace FriendOrganizer.UI.Data
     public interface IFriendDataService
     {
         Task<Friend> GetByIdAsync(int friendId);
+
+        Task SaveAsync(Friend friend);
     }
 }
--- FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.Event;
+using Prism.Commands;
 using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
@@ -22,6 +24,8 @@ namespace FriendOrganizer.UI.ViewModel
             _eventAggregator = eventAggregator;
             _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
                 .Subscribe(OnOpenFriendDetailView);
+
+            SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
         }
 
         public Friend Friend
@@ -34,10 +38,21 @@ namespace FriendOrganizer.UI.ViewModel
             }
         }
 
+        public ICommand SaveCommand { get; }
 
         public async Task LoadAsync(int friendId)
         {
             Friend = await _friendDataService.GetByIdAsync(friendId);
+        }
+
+        private bool OnSaveCanExecute()
+        {
+            return true;
+        }
+
+        private async void OnSaveExecute()
+        {
+          await  _friendDataService.SaveAsync(Friend);
         }
 
         private async void OnOpenFriendDetailView(int friendId)
