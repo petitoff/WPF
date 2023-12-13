586acd6361f5385c30e146d96ad84b058d4f7d2e
Błażej Domagała <petitoff@outlook.com>
2023-12-13 15:20:50 +0100

    Update Configuration.cs file

--- FriendOrganizer/FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer/FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -1,18 +1,15 @@
 ﻿namespace FriendOrganizer.DataAccess.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizerDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        protected override void Seed(FriendOrganizerDbContext context)
         {
             context.Friends.AddOrUpdate(f => f.FirstName,
                 new Model.Friend { FirstName = "Błażej", LastName = "Domagała" },
