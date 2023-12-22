fa6a347dba203ea2e6782ac3c328ea78c7a8dc33
Błażej Domagała <petitoff@outlook.com>
2023-12-22 18:36:01 +0100

    added after module 5

---
+++ WPF and MVVM Test Driven/FriendStorage/FriendStorage.UITests/ViewModel/MainViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using FriendStorage.UI.ViewModel;
+using Moq;
+using Xunit;
+
+namespace FriendStorage.UITests.ViewModel
+{
+    public class MainViewModelTests
+    {
+        private readonly Mock<INavigationViewModel> _navigationViewModelMock;
+        private readonly MainViewModel _viewModel;
+
+        public MainViewModelTests()
+        {
+            _navigationViewModelMock = new Mock<INavigationViewModel>();
+            _viewModel = new MainViewModel(_navigationViewModelMock.Object);
+        }
+
+        [Fact]
+        public void ShouldCallTheLoadMethodOfTheFriendEditViewModel()
+        {
+            _viewModel.Load();
+            _navigationViewModelMock.Verify(vm => vm.Load(), Times.Once);
+        }
+    }
+}
---
+++ WPF and MVVM Test Driven/FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FriendStorage.UI.DataProvider;
+using FriendStorage.UI.ViewModel;
+using Moq;
+using Xunit;
+using FriendStorage.Model;
+
+namespace FriendStorage.UITests.ViewModel
+{
+    public class NavigationViewModelTests
+    {
+        private readonly NavigationViewModel _navigationViewModel;
+
+        public NavigationViewModelTests()
+        {
+            var navigationDataProviderMock = new Mock<INavigationDataProvider>();
+            navigationDataProviderMock.Setup(dp => dp.GetAllFriends()).Returns(
+                new List<LookupItem>
+                {
+                    new LookupItem { Id = 1, DisplayMember = "Julia" },
+                    new LookupItem { Id = 2, DisplayMember = "Thomas" }
+                });
+
+
+            _navigationViewModel = new NavigationViewModel(navigationDataProviderMock.Object);
+            _navigationViewModel.Load();
+        }
+
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            Assert.Equal(2, _navigationViewModel.Friends.Count);
+
+            var friend1 = _navigationViewModel.Friends.SingleOrDefault(f => f.Id == 1);
+            Assert.NotNull(friend1);
+            Assert.Equal("Julia", friend1.DisplayMember);
+
+
+            var friend2 = _navigationViewModel.Friends.SingleOrDefault(f => f.Id == 2);
+            Assert.NotNull(friend2);
+            Assert.Equal("Thomas", friend2.DisplayMember);
+        }
+
+        [Fact]
+        public void ShouldLoadFriendsOnlyOnce()
+        {
+            _navigationViewModel.Load();
+            _navigationViewModel.Load();
+
+            Assert.Equal(2, _navigationViewModel.Friends.Count);
+        }
+    }
+}
